2d4bed2e7a509cba7c5745bce7432378c8659828
xChasingWaves <xchasingwaves@users.noreply.github.com>
2017-05-12 11:56:51 +0200

    Plantilla mejorada

--- Programacion/Unidad07/G1/test.java
+++ Programacion/Unidad07/G1/test.java
@@ -14,10 +14,17 @@ public class test {
 			con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XE");
 			// con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XE", "usuario", "password");
 			stmt = con.createStatement();
-			rs = stmt.executeQuery("SELECT * FROM dept");
+			rs = stmt.executeQuery("SELECT * FROM alum");
+			
+			// Cuenta el número máximo de columnas
+			ResultSetMetaData meta = rs.getMetaData();
+			int maxColumn = meta.getColumnCount();
+			
 			while(rs.next()) {
-				System.out.print(rs.getInt(1) + "\t");
-				System.out.println(rs.getString(2));
+				for (int i = 1; i <= maxColumn; i++) {
+					System.out.print(rs.getString(i) + "\t");
+				}
+				System.out.println("");
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();

4926a68cea7e29707fd310cbfd975c723e5d1637
xChasingWaves <xchasingwaves@users.noreply.github.com>
2017-05-13 14:53:18 +0200

    Ejercicio completado

--- Programacion/Unidad07/G1/Ejercicio07.java
+++ Programacion/Unidad07/G1/Ejercicio07.java
@@ -1,18 +1,96 @@
 package G1;
 
+import java.io.*;
 import java.sql.*;
 
 public class Ejercicio07 {
 	
 	static Connection con = null;
 	static Statement stmt = null;
+	static Statement stmt2 = null;
 	static ResultSet rs = null;
+	static ResultSet rs2 = null;
 	static ResultSetMetaData meta = null;
+	static ResultSetMetaData meta2 = null;
 	static int maxColumn = 0;
+	static int maxColumn2 = 0;
 	
 	
-	public static void listar() {
-		// SIN COMPLETAR
+	public static void listar() throws SQLException {
+		try {
+			String nombre = LibLeer.cadena("Introduce el nombre del archivo (se guardará en modo .txt): ") + ".txt";
+			String directorio = LibLeer.cadena("Introduce la ruta absoluta del directorio donde se guardará el archivo: ");
+			
+			File nuevoArchivo = new File(nombre);
+			File ruta = new File(directorio);
+			
+			while (!ruta.isDirectory() && !ruta.exists()) {
+				directorio = LibLeer.cadena("/!\\ Introduce un directorio que exista: ");
+				ruta = new File(directorio);
+			}
+			
+			File rutaFinal = new File(ruta.getAbsolutePath() + File.separator + nuevoArchivo.getName());
+			
+			FileWriter archivoW = new FileWriter(rutaFinal);
+			BufferedWriter bufferW = new BufferedWriter(archivoW);
+			
+			String sql = "SELECT * FROM emple ORDER BY DEPT_NO";
+			rs = stmt.executeQuery(sql);
+			meta = rs.getMetaData();
+			maxColumn = meta.getColumnCount();
+			
+			sql = "SELECT * FROM depart";
+			rs2 = stmt2.executeQuery(sql);
+			meta2 = rs2.getMetaData();
+			maxColumn2 = meta2.getColumnCount();
+			
+			int totalDept = 0;
+			int totalEmpresa = 0;
+			int esteDept;
+			int empleDept;
+			
+			int i = 1;
+			int j = 1;
+			
+			String cadena = "";
+			while (rs2.next()) {
+				cadena += "Nº Departamento: " + rs2.getString(i) + "\t\tNombre: " + rs2.getString(i+1) + "\t\tLocalidad: " + rs2.getString(i+2) + "\n\n";
+				
+				esteDept = Integer.parseInt(rs2.getString(i));
+				
+				while (rs.next()) {
+					empleDept = Integer.parseInt(rs.getString(j+8));
+					
+					if (esteDept != empleDept) {
+						rs.previous();
+						break;
+					}
+					
+					cadena += "Nº Empleado: " + rs.getString(j) + "\tNombre: " + rs.getString(j+1) + "\nOficio: " + rs.getString(j+2) + "\t\tSalario: " + rs.getString(j+5) + "\t\tTotal: " + rs.getString(j+7) + "\n\n";
+					totalDept += Integer.parseInt(rs.getString(j+7));
+					
+				}
+				
+				cadena += "Suma Total Departamento: " + totalDept + "\n\n\n\n";
+				totalEmpresa += totalDept;
+				totalDept = 0;
+			}
+			
+			cadena += "Suma Total Empresa: " + totalEmpresa;
+			
+			System.out.println(cadena);
+			
+			bufferW.write(cadena);
+			bufferW.flush();
+			bufferW.close();
+			
+			System.out.println("Escritura completada en directorio:\n" + rutaFinal.getAbsolutePath());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
@@ -136,7 +214,8 @@ public class Ejercicio07 {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XE", "ALUMNADODB", "alumnado");
 			// con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XE", "usuario", "password");
-			stmt = con.createStatement();
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt2 = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			
 			int op = 0;
 			do {
@@ -166,6 +245,7 @@ public class Ejercicio07 {
 			
 			if (rs != null) rs.close();
 			if (stmt != null) stmt.close();
+			if (stmt2 != null) stmt2.close();
 			if (con != null) con.close();
 			System.out.println("¡Hasta luego!");
 			
